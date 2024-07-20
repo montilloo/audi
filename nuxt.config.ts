5f4b8dc7dac0752602c19ddc47876cbcad664994
nikun <nikun2008@126.com>
2024-07-20 16:41:10 +0800

    feat: 添加字体;

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,31 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxt/ui', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          'Noto Sans SC': {
+            wght: [100, 300, 400, 500, 700, 900],
+            ital: [100, 300],
+          },
+          Roboto: true,
+        },
+        display: 'swap',
+        preconnect: true,
+        preload: true,
+      },
+    ],
+  ],
   colorMode: {
     preference: 'light',
   },
   tailwindcss: {
     // Options
   },
+  css: ['~/assets/css/main.css'],
 });
