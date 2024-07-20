c56906ed9db496be335a7320dfbd63712e3ef9a9
nikun <nikun2008@126.com>
2024-07-20 16:43:54 +0800

    feat: 添加auth中间件;

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+const isAuthenticated = () => {
+  return false;
+};
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  if (isAuthenticated() === false) {
+    return navigateTo('/login');
+  }
+});
